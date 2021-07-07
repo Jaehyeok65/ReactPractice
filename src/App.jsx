35239c57a70a43354035d84b74fd7ffc48f36d46
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-07-08 03:29:09 +0900

    2021 07 08 생활코딩(컴포넌트와 이벤트 생성)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import Counter from './Components/Counter';
-import NewCounter from './03/NewCounter';
+import Subject from './Components/Subject';
+import TOC from './Components/TOC';
+import Content from './Components/Content';
 import './App.css';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { count : 0 };
-    this.resetCount = this.resetCount.bind(this);
+    this.state = {
+      mode : 'read',
+      welcome : {title : 'WelCome', desc : 'Hello, React!!'},
+      subject : {title : 'WEB', sub : 'World Wid Web!'},
+      contents : [
+        {id : 1, title : 'HTML', desc : 'HTML is for information'},
+        {id : 2, title : 'CSS', desc : 'CSS is for design'},
+        {id : 3, title : 'JavaScript', desc : 'JavaScript is for interactive'}
+      ]
+
+    }
   }
 
-  resetCount() {
-    this.setState(({ count }) => ({ count : count + 10}));
-  }
 
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
-      <div>
-        <div><Counter count = {this.state.count} /></div>
-        <div><NewCounter count = {this.state.count} /></div>
-        <button onClick = {this.resetCount}>{this.state.count+10}으로 초기화</button>
-      </div>
-    );
+      <div className ="App">
+        <Subject 
+        title = {this.state.subject.title}
+        sub = {this.state.subject.sub}
+        onChangePage = {function() {
+          this.setState( {mode : 'welcome'});
+        }.bind(this)}
+        />
+        <TOC onChangePage = {function(){
+          this.setState( { mode : 'read'});
+        }.bind(this)}
+        data = {this.state.contents} />
+        <Content title = {_title} desc = {_desc} />
+         </div>
+    )
+    
   }
 }
 
--- src/Components/Subject.js
+++ src/Components/Subject.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 
 class Subject extends Component {
     render() {
-      const {
-        titlevalue,
-        subtitlevalue,
-      } = this.props;
   
   
       return(
         <header>
-        <h1>{titlevalue}</h1>
-        {subtitlevalue}
+        <h1><a href = "/" onClick = {function(e) {
+          e.preventDefault();
+          this.props.onChangePage();
+        }.bind(this)}>{this.props.title}</a></h1>
+        {this.props.sub}
         </header>
       );
     }
